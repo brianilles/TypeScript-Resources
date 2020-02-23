d43e355684b9c3f84bda003301b28e581473eafd
Brian Illes <brianrilles@gmail.com>
2020-02-23 13:03:13 -0800

    add tutorials

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TypeScript-Resources
+- TypeScript-Crash-Course: https://www.youtube.com/watch?v=rAy_3SIqT-E
+
+- TypeScript-Course-For-Beginners-2020: https://www.youtube.com/watch?v=BwuLxPH8IDs
---
+++ TypeScript-Crash-Course/types.ts
@@ -0,0 +1,42 @@
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+// let strArr: string[];
+let strArr: Array<string>;
+
+// let numArr: number[];
+let numArr: Array<number>;
+
+// let boolArr: boolean[];
+let boolArr: Array<boolean>;
+
+let strNumTuple: [String, number];
+
+myString = 'Hello World';
+myNum = 5 + 5;
+myBool = true;
+myVar = 5;
+
+strArr = ['Hello', 'World'];
+numArr = [1, 2, 4];
+boolArr = [true, false, true];
+
+strNumTuple = ['Hello', 3];
+
+let myVoid: void = null;
+let myNull: null = null;
+let myUndefined: undefined = undefined;
+
+console.log(myString);
+console.log(myNum);
+console.log(myBool);
+console.log(myVar);
+console.log(strArr);
+console.log(numArr);
+console.log(boolArr);
+console.log(strNumTuple);
+console.log(myVoid);
+console.log(myNull);
+console.log(myUndefined);
